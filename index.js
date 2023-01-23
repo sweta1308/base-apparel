012e23c67e1594e03ddbadca43e28d1e9dfe2e27
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-01-23 11:49:50 +0530

    update project

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const submitBtn = document.getElementsByClassName("submit-btn");
-
-submitBtn.addEventListener('click', validateEmail)
-
-function validateEmail() {
-    if (document.getElementsByClassName("input") === " ") {
-        document.getElementsByClassName("error-msg").style.display = "block";
-        document.getElementsByClassName("error-logo").style.display = "block"
-    }
-}
+const submitBtn = document.querySelector(".submit-btn");

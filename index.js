362c44851ded2bb2356479df6c521e5a98efaa52
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-02-08 12:07:13 +0530

    update project

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const submitBtn = document.querySelector(".submit-btn");
 submitBtn.addEventListener("click", validateEmail)
 
 function validateEmail() {
-    var mailFormat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+    var mailFormat = /^\w+([.-]?=\w+)*@\w+([.-]?=\w+)*(.\w{2,3})+$/;
     if (document.querySelector(".input").value.match(mailFormat)) {
         return true;
     } else {
-        document.querySelector(".error-msg").innerText = "Please provide a valid email";
+        document.querySelector(".error-msg").style.display = "block";
+        document.querySelector(".error-msg").innerHTML = "Please provide a valid email";
         document.querySelector(".error-logo").style.display = "block";
         return false;
     }
